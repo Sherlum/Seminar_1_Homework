4dfe2272c60eff616d1c504393f98af1d1769c39
Stanislav <hansolo99@yandex.ru>
2022-06-07 19:52:53 +0300

    1 and 2 exercise complited

---
+++ 1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число большее, а
+// какое меньшее.
+
+Console.WriteLine("Введите первое число");
+string paramString1 = Console.ReadLine();
+int paramInt1 = int.Parse(paramString1);
+Console.WriteLine("Введите второе число");
+string paramString2 = Console.ReadLine();
+int paramInt2 = int.Parse(paramString2);
+Console.WriteLine($"Вы ввели числа {paramInt1} и {paramInt2}");
+
+if (paramInt1 > paramInt2)
+{
+Console.Write($"Число {paramInt1} > Числа {paramInt2}");
+}
+else
+{
+    Console.Write($"Число {paramInt1} < Числа {paramInt2}");
+}
---
+++ 2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int max = 0;
+Console.WriteLine("Введите первое число");
+string paramString1 = Console.ReadLine();
+int paramInt1 = int.Parse(paramString1);
+
+Console.WriteLine("Введите второе число");
+string paramString2 = Console.ReadLine();
+int paramInt2 = int.Parse(paramString2);
+
+Console.WriteLine("Введите третье число");
+string paramString3 = Console.ReadLine();
+int paramInt3 = int.Parse(paramString3);
+
+if (paramInt1 > paramInt2)
+{
+    max = (paramInt1);
+}
+else
+{
+    max = (paramInt2);
+}
+if (max < paramInt3)
+{
+    max = (paramInt3);
+}
+
+Console.WriteLine($"{paramInt1}, {paramInt2}, {paramInt3} -> {max}");
