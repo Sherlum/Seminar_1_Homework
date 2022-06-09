4b7ff3d24da156fe84491ddac2f7449987f96ea5
Stanislav <hansolo99@yandex.ru>
2022-06-09 20:10:01 +0300

    Exercise 4 complited

---
+++ 4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число (N) и выдает все четные числа между ними от 1
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+int i = 2;
+while (i <= numberInt)
+{
+    Console.Write($"{i} ");
+    i += 2;
+}
