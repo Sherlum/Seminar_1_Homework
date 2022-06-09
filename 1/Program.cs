18fb6c5f58c61076ae05f64840a81d6a01ea07ad
Stanislav <hansolo99@yandex.ru>
2022-06-09 14:44:44 +0300

    Exersice 1 fixed

--- 1/Program.cs
+++ 1/Program.cs
@@ -10,11 +10,16 @@ string paramString2 = Console.ReadLine();
 int paramInt2 = int.Parse(paramString2);
 Console.WriteLine($"Вы ввели числа {paramInt1} и {paramInt2}");
 
-if (paramInt1 > paramInt2)
-{
-Console.Write($"Число {paramInt1} > Числа {paramInt2}");
-}
+    if (paramInt1 == paramInt2)
+     Console.Write($"{paramInt1} = {paramInt2}");
 else
 {
-    Console.Write($"Число {paramInt1} < Числа {paramInt2}");
+            if (paramInt1 > paramInt2)
+            {
+            Console.Write($"Число {paramInt1} > Числа {paramInt2}");
+            }
+        else
+        {
+        Console.Write($"Число {paramInt1} < Числа {paramInt2}");
+        }
 }
